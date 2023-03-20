4ac214543f334275e962271e6efd8cb8b1e3c4ac
Chidinma Jewel <jewelchidinma@gmail.com>
2023-03-20 11:59:47 +0100

    env

--- App/socket/Helper.js
+++ App/socket/Helper.js
@@ -13,12 +13,6 @@ const menuOptions = [
 	'2. Chicken Pie',
 	'3. Beef Pie',
 	'4. Fish Pie',
-	// '5. Vegetable Pie',
-	// '6. Chicken and Chips',
-	// '7. Beef and Chips',
-	// '8. Fish and Chips',
-	// '9. Vegetable and Chips',
-	// '10. Chicken and Rice',
 ];
 
 module.exports = { formatMessage, menuOptions };
